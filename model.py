c17734f9fab93675ca96f17e63d4fe325a966960
aheschl1 <ajheschl@gmail.com>
2025-02-28 11:30:58 -0700

    the grads prop!

--- model.py
+++ model.py
@@ -22,17 +22,18 @@ class NeuralMemory(nn.Module):
                 lr: float=1e-3
             ):
         super(NeuralMemory, self).__init__()
-        self.model = nn.Linear(dim_in, dim_out, bias=True)        
+        self.register_buffer("model", torch.empty(dim_out, dim_in, requires_grad=True, dtype=torch.float32))
+        nn.init.xavier_normal_(self.model)
+                   
         self.key = nn.Linear(dim_in, dim_in, bias=False)
         self.value = nn.Linear(dim_in, dim_in, bias=False)
         self.query = nn.Linear(dim_in, dim_in, bias=False)
         
         self.st_past_state = BufferedState()      
-        self.lr = lr  
-        self.surprise_metric = nn.L1Loss(reduction='mean')
-        self.model.requires_grad_(False)
+        self.lr = torch.tensor(lr)  
+        self.surprise_metric = nn.L1Loss(reduction='sum')
     
-    def _update_memory(self, grads, eta, alpha):
+    def _update_memory(self, grad, eta, alpha):
         """
         Optimizer for the neural memory.
         
@@ -46,22 +47,21 @@ class NeuralMemory(nn.Module):
         - eta: data dependent momentum
         - alpha: decay factor
         """
-        for (pname, param), grad in zip(self.model.named_parameters(), grads):
-            past_surprise = self.st_past_state.get_value(pname)
-            # check state for this params and initialize if not present
-            if past_surprise is None:
-                past_surprise = torch.zeros_like(param.data)
-                self.st_past_state.register(pname, past_surprise)
-                
-            if grad is None:
-                warning(f"Gradient for {pname} is None. Skipping update.")
-                continue
+        past_surprise = self.st_past_state.get_value("weight")
+        # check state for this params and initialize if not present
+        if past_surprise is None:
+            past_surprise = torch.zeros_like(self.model)
+            self.st_past_state.register("weight", past_surprise)
             
-            surpise = eta*past_surprise + self.lr*grad # surprise_t = eta*surprise_{t-1} + lr*grad
-            
-            self.st_past_state.register_buffer(pname, surpise)
-            # now weights with decay. (1-alpha)*param + surprise
-            param.data = (1-alpha)*param.data + surpise
+        if grad is None:
+            warning(f"Gradient for weight is None. Skipping update.")
+        
+        surpise = eta*past_surprise + self.lr*grad # surprise_t = eta*surprise_{t-1} + lr*grad
+        
+        self.st_past_state.register_buffer("weight", surpise)
+        # now weights with decay. (1-alpha)*param + surprise
+        self.model = self.model*(1-alpha) + surpise
+
                 
     def condition(self, x) -> torch.Tensor:
         """
@@ -70,38 +70,36 @@ class NeuralMemory(nn.Module):
         Returns:
         - surprise: the surprise metric
         """
-        self.model.requires_grad_(True)
         # prepare the grad. inner loop only updates the model
         k = self.key(x)
         v = self.value(x)
         
-        s_t = self.surprise_metric(self.model(k), v)
+        s_t = self.surprise_metric((self.model@k.permute(1,0)).permute(1,0), v) # L1Loss
         # Compute gradients w.r.t. model params
-        grads = torch.autograd.grad(s_t, self.model.parameters(), create_graph=True, retain_graph=True)
-        self._update_memory(grads, eta=0.9, alpha=0.1)
-        self.model.requires_grad_(False)
-        return s_t 
+        grads = torch.autograd.grad(s_t, self.model, create_graph=True, retain_graph=True)[0]
+        self._update_memory(grads, eta=torch.tensor(0.9), alpha=torch.tensor(0.1))
+        return s_t
 
-    
     def forward(self, x):
         q = self.query(x)
-        return self.model(q)
+        return (self.model@q.permute(1,0)).permute(1,0)
     
 if __name__ == "__main__":
-    x = torch.randn(1, 10) # tokens 1 x 10
+    x = torch.randn(12, 10) # tokens 1 x 10
     model = NeuralMemory(dim_in=10, dim_out=10)
     
-    s = model.condition(x)
-    y = model(x)
-    
-    l = nn.L1Loss()
-    loss = l(y, x)
-    loss.backward()
-    
-    print(model.key.weight.grad)
+    model.condition(x)
+    model.condition(x)
+    model.condition(x)
+
+    y = model(x) 
+    loss = nn.L1Loss()(y, x)
+
+    # loss.backward() # d(MQx - x)/dw1   
     print(model.value.weight.grad)
-    print(model.query.weight.grad)    
+    print(model.key.weight.grad)  
+    print(model.query.weight.grad)
     
-    # vis = make_dot(loss, params=dict(model.named_parameters()), show_attrs=True, show_saved=True)
+    vis = make_dot(loss, params=dict(model.named_parameters()), show_saved=True)
     # save
-    # vis.render("model", format="png", cleanup=True)
+    vis.render("model", format="png", cleanup=True)
