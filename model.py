a40168601765cbf2c5cae337287dd83660a53397
aheschl1 <ajheschl@gmail.com>
2025-03-01 16:02:45 -0700

    extract memory to a separate class

--- model.py
+++ model.py
@@ -1,39 +1,47 @@
+from abc import ABC, abstractmethod
 from logging import warning
 import torch
 import torch.nn as nn
+from typing import List, Tuple
 from torch.optim import Optimizer
 from torchviz import make_dot
 
 
-class BufferedState(nn.Module):
-    def __init__(self):
-        super(BufferedState, self).__init__()
+class MemoryModule(ABC, nn.Module):
+    def __init__(self, dim_in, dim_out, lr):
+        """
+        Memory module for the neural memory.
+        """
+        super(MemoryModule, self).__init__()
+        self.lr = torch.tensor(lr)
+        self.dim_in = dim_in
+        self.dim_out = dim_out
+        self._construct_layers()
         
-    def register(self, key, value):
-        self.register_buffer(key, value)
+    @abstractmethod
+    def construct_layers(self) -> List[Tuple[str, torch.Tensor]]:
+        """
+        Create the weights for the model. This is an abstract method that should be implemented by the subclass.
         
-    def get_value(self, key):
-        return getattr(self, key, None)
-
-class NeuralMemory(nn.Module):
-    def __init__(self, 
-                dim_in: int, 
-                dim_out: int, 
-                lr: float=1e-3
-            ):
-        super(NeuralMemory, self).__init__()
-        self.register_buffer("model", torch.empty(dim_out, dim_in, requires_grad=True, dtype=torch.float32))
-        nn.init.xavier_normal_(self.model)
-                   
-        self.key = nn.Linear(dim_in, dim_in, bias=False)
-        self.value = nn.Linear(dim_in, dim_in, bias=False)
-        self.query = nn.Linear(dim_in, dim_in, bias=False)
-        
-        self.st_past_state = BufferedState()      
-        self.lr = torch.tensor(lr)  
-        self.surprise_metric = nn.L1Loss(reduction='sum')
+        Returns:
+        - List of tuples of the form (name, weight) where name is the name of the weight and weight is the tensor.
+        """
+        ...
     
-    def _update_memory(self, grad, eta, alpha):
+    @abstractmethod
+    def forward(self, x: torch.Tensor)->torch.Tensor:
+        ...
+        
+    def _construct_layers(self):
+        """
+        Create the buffers for the model. Also create the surprise buffers. One for each weight.
+        """
+        weights = self.construct_layers()
+        for name, weight in weights:
+            self.register_buffer(name, weight)
+            self.register_buffer(MemoryModule._get_corresponding_surprise_name(name), torch.zeros_like(weight, requires_grad=False))
+            
+    def _update_memory(self, grads, eta, alpha):
         """
         Optimizer for the neural memory.
         
@@ -47,22 +55,83 @@ class NeuralMemory(nn.Module):
         - eta: data dependent momentum
         - alpha: decay factor
         """
-        past_surprise = self.st_past_state.get_value("weight")
-        # check state for this params and initialize if not present
-        if past_surprise is None:
-            past_surprise = torch.zeros_like(self.model)
-            self.st_past_state.register("weight", past_surprise)
-            
-        if grad is None:
-            warning(f"Gradient for weight is None. Skipping update.")
+        if len(grads) != len(self.get_named_weights()):
+            raise ValueError(f"Number of gradients {len(grads)} does not match number of weights {len(self.get_named_weights())}")
         
-        surpise = eta*past_surprise + self.lr*grad # surprise_t = eta*surprise_{t-1} + lr*grad
+        for grad, (name, weight) in zip(grads, self.get_named_weights()):
+            sname = MemoryModule._get_corresponding_surprise_name(name)
+            # get the past surprise for this weight
+            past_surprise = self.get_buffer(sname)
+            if grad is None:
+                warning(f"Gradient for weight is None. Skipping update.")
+                continue
+            surpise = eta*past_surprise + self.lr*grad # surprise_t = eta*surprise_{t-1} + lr*grad
+            self.register_buffer(sname, surpise)
+            self.register_buffer(name, (1-alpha)*weight + surpise)
+    
+    def update(self, grads, eta, alpha):
+        """
+        Update the memory with the gradients
         
-        self.st_past_state.register_buffer("weight", surpise)
-        # now weights with decay. (1-alpha)*param + surprise
-        self.model = self.model*(1-alpha) + surpise
+        Args:
+        - grads: the gradients
+        - eta: data dependent momentum
+        - alpha: decay factor
+        """
+        self._update_memory(grads, eta, alpha)
+    
+    @staticmethod
+    def _get_corresponding_surprise_name(name):
+        """
+        Get the name of the surprise buffer corresponding to a weight name.
+        """
+        return f"{name}_surprise_tpast"
+        
+    def get_named_weights(self) -> List[Tuple[str, torch.Tensor]]:
+        return [(name, weight) for name, weight in self.named_buffers() if "surprise" not in name]
+    
+    def get_weights(self) -> List[torch.Tensor]:
+        return [weight for _, weight in self.get_named_weights()]
+    
+    def __call__(self, *args, **kwds):
+        return self.forward(*args, **kwds)
+        
+class LinearMemory(MemoryModule):
+    """
+    A linear memory module, which is a simple matrix multiplication. No bias. No activation.
+    """
+    def construct_layers(self) -> List[Tuple[str, torch.Tensor]]:
+        w = torch.empty(self.dim_out, self.dim_in, requires_grad=True, dtype=torch.float32)
+        nn.init.xavier_normal_(w)
+        return [("model", w)]
+        
+    def forward(self, x: torch.Tensor)->torch.Tensor:
+        """
+        Compute the forward pass of the model.
+        
+        Args:
+        - x: input tensor of shape (batch_size, dim_in)
+        
+        Returns:
+        - y: output tensor of shape (batch_size, dim_out)
+        """
+        return (self.model@x.permute(1,0)).permute(1,0)
+    
 
-                
+class NeuralMemory(nn.Module):
+    def __init__(self, 
+                dim_in: int, 
+                dim_out: int, 
+                lr: float=1e-3
+            ):
+        super(NeuralMemory, self).__init__()
+        self.memory = LinearMemory(dim_in, dim_out, lr)
+        self.key = nn.Linear(dim_in, dim_in, bias=True)
+        self.value = nn.Linear(dim_in, dim_in, bias=True)
+        self.query = nn.Linear(dim_in, dim_in, bias=True)
+        
+        self.surprise_metric = nn.L1Loss(reduction='sum')
+           
     def condition(self, x) -> torch.Tensor:
         """
         Condition the model on the input x
@@ -74,24 +143,22 @@ class NeuralMemory(nn.Module):
         k = self.key(x)
         v = self.value(x)
         
-        s_t = self.surprise_metric((self.model@k.permute(1,0)).permute(1,0), v) # L1Loss
+        s_t = self.surprise_metric(self.memory(k), v) # L1Loss
         # Compute gradients w.r.t. model params
-        grads = torch.autograd.grad(s_t, self.model, create_graph=True, retain_graph=True)[0]
-        self._update_memory(grads, eta=torch.tensor(0.9), alpha=torch.tensor(0.1))
+        grads = torch.autograd.grad(s_t, self.memory.get_weights(), create_graph=True, retain_graph=True)
+        self.memory.update(grads, eta=torch.tensor(0.9), alpha=torch.tensor(0.1))
         return s_t
 
     def forward(self, x):
-        q = self.query(x)
-        return (self.model@q.permute(1,0)).permute(1,0)
+        return self.memory(self.query(x))
     
 if __name__ == "__main__":
-    x = torch.randn(12, 10) # tokens 1 x 10
+    x = torch.randn(12, 10, device="cuda") # tokens 1 x 10
     model = NeuralMemory(dim_in=10, dim_out=10)
+    model = model.to("cuda")
     
     model.condition(x)
-    model.condition(x)
-    model.condition(x)
-
+    
     y = model(x) 
     loss = nn.L1Loss()(y, x)
 
